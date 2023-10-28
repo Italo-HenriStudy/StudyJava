6089c9a58d16004870d7346adccdb598f5d269d6
Italo-Henri <italohenrique655@gmail.com>
2023-10-27 23:26:29 -0300

    Estudando Modificadores de Acesso

--- src/academy/devdojo/maratonaviradonojiraya/javacore/introducaometodos/dominio/Employee.java
+++ src/academy/devdojo/maratonaviradonojiraya/javacore/introducaometodos/dominio/Employee.java
@@ -1,30 +1,51 @@
 package academy.devdojo.maratonaviradonojiraya.javacore.introducaometodos.dominio;
+
 import java.math.BigDecimal;
 
 public class Employee {
-    public String name;
-    public int age;
-    public BigDecimal[] wage;
+    private String name;
+    private int age;
+    private BigDecimal[] wage;
 
     public void printEmployeeData() {
         System.out.println("Name: " + this.name);
         System.out.println("Age: " + this.age);
         int i = 0;
-        for (BigDecimal num :
-                wage) {
-            System.out.println("Salary[" + i + "]: " + num);
+        if (wage == null) return;
+        for (BigDecimal salary :
+                this.wage) {
+            System.out.println("Salary[" + i + "]: " + salary);
             i++;
         }
         System.out.println("Average Salary: " + averageSalary(this.wage));
     }
-    public BigDecimal averageSalary(BigDecimal... wage) {
+
+    private BigDecimal averageSalary(BigDecimal... wage) {
         BigDecimal sumWage = BigDecimal.valueOf(0);
         BigDecimal average;
-        for (BigDecimal num :
-                wage) {
-            sumWage = sumWage.add(num);
+        if (wage == null) return sumWage;
+        for (BigDecimal salary :
+                this.wage) {
+            sumWage = sumWage.add(salary);
         }
         average = sumWage.divideToIntegralValue(BigDecimal.valueOf(wage.length));
         return average;
     }
+
+    public String getName() {return this.name;}
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {return this.age;}
+    public void setAge(int age) {
+        if (age < 0) {
+            System.out.println("Please, enter a valid age!");
+            return;
+        }
+        this.age = age;
+    }
+
+    public BigDecimal[] getWage() {return this.wage;}
+    public void setWage(BigDecimal... wage) {this.wage = wage;}
 }
--- src/academy/devdojo/maratonaviradonojiraya/javacore/introducaometodos/test/EmployeeTest01.java
+++ src/academy/devdojo/maratonaviradonojiraya/javacore/introducaometodos/test/EmployeeTest01.java
@@ -1,16 +1,19 @@
 package academy.devdojo.maratonaviradonojiraya.javacore.introducaometodos.test;
 import academy.devdojo.maratonaviradonojiraya.javacore.introducaometodos.dominio.Employee;
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 public class EmployeeTest01 {
     public static void main(String[] args) {
         Employee employee = new Employee();
 
-        employee.name = "GuiZika Gamesplay";
-        employee.age = 20;
-        employee.wage = new BigDecimal[]{BigDecimal.valueOf(5000.0), BigDecimal.valueOf(3500.0), BigDecimal.valueOf(2500.0)};
+        employee.setName("João Pé de Feijão");
+        employee.setAge(30);
+        employee.setWage(BigDecimal.valueOf(5000.0), BigDecimal.valueOf(3500.0), BigDecimal.valueOf(2500.0));
 
-        employee.printEmployeeData();
-
+//        employee.printEmployeeData();
+        System.out.println(employee.getName());
+        System.out.println(employee.getAge());
+        System.out.println(Arrays.toString(employee.getWage()));
     }
 }
